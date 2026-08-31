576da31d2a39c139bd9d865bf8306046159307c3
Paul Flores <paulfloresdev@icloud.com>
2026-08-31 15:54:25 -0600

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,95 @@
-import Image from "next/image";
+import Link from "next/link";
+import { Calendar, ChevronRight} from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+    <div className="
+              grid grid-cols-2 
+              gap-4  
+              bg-[url('/PageBackground.png')]
+              bg-no-repeat
+              bg-bottom-left
+              bg-contain">
+      <div className="
+              w-fit 
+              mx-auto 
+              text-left
+              ">
+
+        <h2 className="text-2xl font-bold text-green-500">
+          RECUPERA TU MOVIMIENTO
+        </h2>
+
+        <h1 className="text-6xl font-bold">
+          Vuelve a moverte
+          <br />
+          <div className="text-green-700">
+            sin dolor
+          </div>
+        </h1>
+
+        <p className="text-9xl2xl font-semibold mt-4">
+          Fisioterapia personalizada para aliviar el dolor,
+          <br/>
+          recuperar tu movilidad y volver a disfrutar tu vida
+        </p>
+
+        <div className="mt-8 flex flex-col sm:flex-row items-center gap-4 font-bold">
+           <Link
+              href="/agenda"
+              className="
+                flex
+                items-center
+                gap-2
+                bg-orange-400
+                hover:bg-orange-500
+                p-4
+                rounded-lg
+                text-white
+                font-medium
+                transition-colors
+              "
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+              <Calendar className="w-7 h-7 text-white" />
+
+              <span>
+                Agenda tu valoración 
+              </span>
+
+               <ChevronRight className="w-5 h-5 text-white" />
+            </Link>
+
+          <Link 
+            href="/paquetes" 
+            className="
+              flex
+              items-center
+              ml-4
+              p-4
+              border-3
+              rounded-xl
+              border-black-100
+              hover:border-black-200
+
+            ">
+            Conoce los tratamientos 
+            <ChevronRight className="w-5 h-5 text-black" />
+          </Link>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+
+      </div>
+      
+      <div className="bg-green-500">
+        Second div
+      </div>
+
+      <div className="col-span-2 bg-yellow-500">
+        third <div></div>
+      </div>
+    
     </div>
+    
+
+
   );
 }
