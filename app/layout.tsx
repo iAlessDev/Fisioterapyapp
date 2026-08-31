825642998383403197ad50abbe23c5a4b13abba2
Paul Flores <paulfloresdev@icloud.com>
2026-08-31 15:53:58 -0600

    change to main global structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,8 +25,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
         <Header />
-        {children}
-        </body>
+        <main>
+        {children}  
+        </main>
+      </body>
     </html>
   );
 }
